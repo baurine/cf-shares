2f9137e9941088a55d3389e8a3a86172a7998eb3
baurine <2008.hbl@gmail.com>
2018-05-08 22:27:40 +0800

    version 2

--- workshop.js
+++ workshop.js
@@ -1,39 +1,57 @@
 
-let fridge = []
-let fridge_open = false
-
-function open_fridge() {
-  fridge_open = true
+let fridge_1 = {
+  icebox: [],
+  door_open: false
+}
+let fridge_2 = {
+  icebox: [],
+  door_open: false
 }
 
-function add_to_fridge(obj) {
-  if (fridge_open) {
-    fridge.push(obj)
+function open_fridge(fridge) {
+  fridge.door_open = true
+}
+
+function add_to_fridge(fridge, obj) {
+  if (fridge.door_open) {
+    fridge.icebox.push(obj)
     console.log('success');
   } else {
     console.log('failed, the fridge is closed');
   }
 }
 
-function close_fridge() {
-  fridge_open = false
+function close_fridge(fridge) {
+  fridge.door_open = false
 }
 
-function inspect_fridge() {
-  console.log(fridge);
+// 需要知道 fridge 的细节，而面向对象，无须，从而体现它的封装性
+// 你需要知道，冰箱有个门，可以开关，里面有个 icebox，可以用来装东西
+function inspect_fridge(fridge) {
+  console.log(fridge.icebox);
 }
 
 function main() {
   console.log('start');
 
-  open_fridge();
-  add_to_fridge('🐈');
-  add_to_fridge('🐈');
-  close_fridge();
+  open_fridge(fridge_1);
+  add_to_fridge(fridge_1, '🐈');
+  add_to_fridge(fridge_1, '🐈');
+  close_fridge(fridge_1);
 
-  add_to_fridge('🐈');
+  add_to_fridge(fridge_1, '🐈');
 
-  inspect_fridge();
+  inspect_fridge(fridge_1);
+
+  open_fridge(fridge_2);
+  add_to_fridge(fridge_2, '🐶');
+  add_to_fridge(fridge_2, '🐶');
+  add_to_fridge(fridge_2, '🐶');
+  close_fridge(fridge_2);
+
+  add_to_fridge(fridge_2, '🐶');
+
+  inspect_fridge(fridge_2);
 }
 
 main()
