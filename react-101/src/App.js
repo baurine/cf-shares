efb26aa9215ee1a39683366359c477e1b92d49f4
baurine <2008.hbl@gmail.com>
2018-05-23 17:29:19 +0800

    4 kinds of implementation for Hello component

--- react-101/src/App.js
+++ react-101/src/App.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function Hello(props) {
+//   return <h1>Hello {props.name}</h1>
+// }
+
+// const Hello = ({props}) => (
+//   <h1>Hello {props.name}</h1>
+// )
+
+const Hello = ({name}) => (
+  <h1>Hello {name}</h1>
+)
+
+// class Hello extends Component {
+//   render() {
+//     return <h1>Hello {this.props.name}</h1>
+//   }
+// }
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Hello name='ekohe'/>
     );
   }
 }
